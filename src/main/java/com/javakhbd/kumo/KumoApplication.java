988093999180de31a365d50c28e624e5cf23306c
liuzhipeng <492782442@qq.com>
2019-07-27 13:51:51 +0800

    Initial commit

---
+++ src/main/java/com/javakhbd/kumo/KumoApplication.java
@@ -0,0 +1,13 @@
+package com.javakhbd.kumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KumoApplication.class, args);
+    }
+
+}
